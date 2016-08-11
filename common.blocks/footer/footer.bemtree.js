6ef085d886d8e45c0552b11fd0244260f29dac9d
Ivan Soldatov <yazvyazda@mail.ru>
2016-08-11 16:07:26 +0300

    add group block

--- common.blocks/footer/footer.bemtree.js
+++ common.blocks/footer/footer.bemtree.js
@@ -1,18 +1,28 @@
 block('footer').content()(function() {
     return [
         {
-            block: 'phone',
-            mix: { block: 'footer', elem: 'phone' },
-            mods: { icon: true }
+            block: 'logo',
+            mix: { block: 'footer', elem: 'logo' }
         },
         {
-            block: 'email',
-            mix: { block: 'footer', elem: 'email' },
-            mods: { icon: true }
-        },
-        {
-            elem: 'adress',
-            mods: { icon: true }
+            elem: 'contacts',
+            mix: { block: 'group', mods: { col: true } },
+            content: [
+                {
+                    block: 'phone',
+                    mix: { block: 'footer', elem: 'phone' },
+                    mods: { icon: true }
+                },
+                {
+                    block: 'email',
+                    mix: { block: 'footer', elem: 'email' },
+                    mods: { icon: true }
+                },
+                {
+                    elem: 'adress',
+                    mods: { icon: true }
+                }
+            ]
         }
     ];
 });
--- common.blocks/footer/footer.deps.js
+++ common.blocks/footer/footer.deps.js
@@ -1,5 +1,6 @@
 ({
     shouldDeps: [
+      'group',
       'phone',
       'email'
     ]
