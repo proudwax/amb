fee2417d87730944273650269b0f346ff33ff3f5
Ivan Soldatov <yazvyazda@mail.ru>
2016-09-19 12:09:12 +0300

    fix

--- common.blocks/goods-list/goods-list.js
+++ common.blocks/goods-list/goods-list.js
@@ -15,6 +15,8 @@ provide(BEMDOM.decl(this.name, {
 		console.log(this);
 		console.log(this.elem('item')[1]);
 
+		_this.setMod(_this.elem('item')[1], 're', true);
+
 		this.elem('item').map(function(item) {
 			if((_this.elem('item').length - lastCount) < item){
 				_this.delMod(_this.elem('item')[item], 're');
